a2e75a834983141e915008270dbb8345200146b9
Karthick-Ramesh-16 <karthick.ramesh@freshworks.com>
2022-05-07 00:49:03 +0530

    Remove tabs

--- Doubly Linked List/DoublyLinkedListNode.h
+++ Doubly Linked List/DoublyLinkedListNode.h
@@ -1,17 +1,16 @@
 class Node {
-	int data;
-	Node *next, *previous;
+	int data; Node *next, *previous;
 
 	public:
 
-	Node(int val) {	data = val;	next = NULL;	previous = NULL;	}
+	Node(int val) { data = val; next = NULL; previous = NULL; }
 
-	void setData(int val) {  data = val; }
-	int getData() { return data;  }
+	void setData(int val) { data = val; }
+	int getData() { return data; }
 
-	void setNext(Node* link) {  next = link;  }
-	Node* getNext() { return next;  }
+	void setNext(Node* link) { next = link; }
+	Node* getNext() { return next; }
 
-	void setPrevious(Node* link) {  previous = link;  }
-	Node* getPrevious() { return previous;  }
+	void setPrevious(Node* link) { previous = link; }
+	Node* getPrevious() { return previous; }
 };
--- Doubly Linked List/DoublyLinkedListProgram.cpp
+++ Doubly Linked List/DoublyLinkedListProgram.cpp
@@ -13,15 +13,15 @@ int main() {
 		cin >> n;
 		switch(n) {
 			case 0: break;
-			case 1:	cout << "Enter a value: ";  cin >> ip;
+			case 1: cout << "Enter a value: "; cin >> ip;
 				dl.InsertFront(ip); break;
-			case 2:	cout << "Enter a value: ";  cin >> ip;
+			case 2: cout << "Enter a value: "; cin >> ip;
 				dl.InsertEnd(ip); break;
-			case 3:	cout << "Enter the value: ";  cin >> afv;
+			case 3: cout << "Enter the value: "; cin >> afv;
 				cout << "Enter a value to insert: "; cin >> ip;
 				dl.InsertAfterValue(ip, afv); break;
-			case 4:	dl.display(); break;
-			default:	cout << "Invalid input" << endl;
+			case 4: dl.display(); break;
+			default: cout << "Invalid input" << endl;
 		}
 	} while (n != 0);
 	cout << "Done" << endl;
