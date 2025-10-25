9da93d3d92a9113d6364850e91c18ccefbeda7a7
Karthick-sketch <karthick246.kh@outlook.com>
2025-10-25 16:00:47 +0000

    Implement Merge sort

--- Java/Sort/src/Sort.java
+++ Java/Sort/src/Sort.java
@@ -1,39 +1,81 @@
 public class Sort {
 
-  public static void bubbleSort(int[] arr) {
-    for (int i = 1; i < arr.length; i++) {
-      for (int j = 0; j < arr.length - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-          int v = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = v;
+  public static void bubbleSort(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      for (int j = 0; j < array.length - i; j++) {
+        if (array[j] > array[j + 1]) {
+          int v = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = v;
         }
       }
     }
   }
 
-  public static void selectionSort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
+  public static void selectionSort(int[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
       int min = i;
-      for (int j = i + 1; j < arr.length; j++) {
-        if (arr[j] < arr[min]) {
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[j] < array[min]) {
           min = j;
         }
       }
-      int temp = arr[min];
-      arr[min] = arr[i];
-      arr[i] = temp;
+      int temp = array[min];
+      array[min] = array[i];
+      array[i] = temp;
     }
   }
 
-  public static void insertionSort(int[] arr) {
-    for (int i = 1; i < arr.length; i++) {
-      int temp = arr[i];
+  public static void insertionSort(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      int temp = array[i];
       int j = i - 1;
-      for (; j >= 0 && arr[j] > temp; j--) {
-        arr[j + 1] = arr[j];
+      for (; j >= 0 && array[j] > temp; j--) {
+        array[j + 1] = array[j];
       }
-      arr[j + 1] = temp;
+      array[j + 1] = temp;
     }
+  }
+
+  public static void mergeSort(int[] array) {
+    int len = array.length;
+    if (len <= 1) {
+      return;
+    }
+    int mid = len / 2;
+    int[] left = new int[mid];
+    int[] right = new int[len - mid];
+    int i = 0;
+    for (int l = 0; i < mid; l++, i++) {
+      left[l] = array[i];
+    }
+    for (int r = 0; i < len; r++, i++) {
+      right[r] = array[i];
+    }
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, array);
+  }
+
+  private static int[] merge(int[] left, int[] right, int[] array) {
+    int lSize = array.length / 2;
+    int rSize = array.length - lSize;
+    int i = 0, l = 0, r = 0;
+    for (; l < lSize && r < rSize; i++) {
+      if (left[l] < right[r]) {
+        array[i] = left[l];
+        l++;
+      } else {
+        array[i] = right[r];
+        r++;
+      }
+    }
+    for (; l < left.length; l++, i++) {
+      array[i] = left[l];
+    }
+    for (; r < right.length; r++, i++) {
+      array[i] = right[r];
+    }
+    return array;
   }
 }
--- Java/Sort/src/SortOperations.java
+++ Java/Sort/src/SortOperations.java
@@ -19,6 +19,7 @@ public class SortOperations {
   private static void operation(int[] arr1) {
     int[] arr2 = Arrays.copyOf(arr1, arr1.length);
     int[] arr3 = Arrays.copyOf(arr1, arr1.length);
+    int[] arr4 = Arrays.copyOf(arr1, arr1.length);
     System.out.println("Unsorted:\t" + Arrays.toString(arr1));
     Sort.bubbleSort(arr1);
     System.out.println("Bubble sort:\t" + Arrays.toString(arr1));
@@ -26,5 +27,7 @@ public class SortOperations {
     System.out.println("Selection sort:\t" + Arrays.toString(arr2));
     Sort.insertionSort(arr3);
     System.out.println("Insertion sort:\t" + Arrays.toString(arr3));
+    Sort.mergeSort(arr4);
+    System.out.println("Merge sort:\t" + Arrays.toString(arr4));
   }
 }
